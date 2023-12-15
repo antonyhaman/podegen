f5b27fe10066fefaf521f9ff53702d7eee36119d
Anton Haman <hamana2@medtronic.com>
2023-12-15 18:35:25 +0200

    Remove unsued string utility function

--- podegen-core/src/main/java/com/github/kotvertolet/podegen/core/utils/StringUtils.java
+++ podegen-core/src/main/java/com/github/kotvertolet/podegen/core/utils/StringUtils.java
@@ -2,20 +2,6 @@ package com.github.kotvertolet.podegen.core.utils;
 
 public class StringUtils {
 
-    public static String capitalizeStr(String string) {
-        char[] chars = string.toLowerCase().toCharArray();
-        boolean found = false;
-        for (int i = 0; i < chars.length; i++) {
-            if (!found && Character.isLetter(chars[i])) {
-                chars[i] = Character.toUpperCase(chars[i]);
-                found = true;
-            } else if (Character.isWhitespace(chars[i]) || chars[i] == '.' || chars[i] == '\'') { // You can add other chars here
-                found = false;
-            }
-        }
-        return String.valueOf(chars);
-    }
-
     public static String capitalize(String str) {
         int strLen = str != null ? str.length() : 0;
         if (strLen == 0) {
